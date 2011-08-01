df8b95d4ef25f85236a52206574d2fc06fea5334
Raoul Millais <raoul@raoulmillais.com>
2011-08-01 09:23:15 +0100

    Split up actions and added user info page for successful connections, fork action to fork the repo.  Added user object and implemented session storage of access tokens and users.

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var express = require('express'),
 	http = require('http'),
 	jade = require('jade'),
 	config = require('./config').config,
+	User = require('./lib/user').User,
 	GitHubApi = require('./support/github/lib/github').GitHubApi,
 	server;
 
 server = express.createServer();
+server.use(express.cookieParser());
+server.use(express.session({ secret: 'random' }));
 
 server.get('/', function showHomePage(req, res) {
 	res.render('index.jade', {});
@@ -15,48 +18,77 @@ server.get('/', function showHomePage(req, res) {
 server.get('/github/authorised', function storeToken(req, res) {
 	var url = require('url').parse(req.url),
 		qs= require('querystring').parse(url.query),
-		github = new GitHubApi(true);
+		github = new GitHubApi(true),
+		user;
 
-	// TODO: Create a user with the access code and put it in session
+	if (!qs.code) {
+			console.error('Error getting access token: %j', tokenErr);
+			res.redirect('/github/error');
+			return;
+	}
+
+	req.session.user = user = new User(qs.code);
+
 	github.getOAuthAccessToken(qs.code, config.githubapplicationclientid, config.githubclientsecret, 
 								function handleTokenResponse(tokenErr, tokenResponse) {
 		var accessToken,
 			username;
 
-		if (tokenErr) {
-			console.error('Error getting access token: %j', tokenErr);
+		if (tokenErr || !tokenResponse.access_token) {
+			console.error('Error getting access token: %j', tokenErr || tokenResponse);
 			res.redirect('/github/error');
 			return;
 		}
 
-		accessToken = tokenResponse.access_token,
+		req.session.accessToken = accessToken = tokenResponse.access_token;
 		console.log('Received access token');
-		// TODO: Store the access token in User in session
-		// TODO: Redirect to create store page
+		res.redirect('/github/user');
 
-		github.authenticateOAuth(accessToken);
-		
-		github.getUserApi().show('', function handleShowUserResponse(userErr, userResponse) {
-			if (userErr) {
-				console.error('Error getting user: %j', userErr);
-			}
-			console.log('Received user info: %j', userResponse);
-			// TODO: Create a user and store the access code
-			// TODO: Store the token in session
-		});
+	});
 
-		// TODO: Check whether the user has already forked the repository
-		// TODO: Get the repository to fork from config
-		github.getRequest().post('repos/fork/7digital/7digital-python-api-wrapper',
-									{} /* postParameters */, null /* requestOptions */,
-									function handlerForkResponse(forkError, forkResponse) {
-			if (forkError) {
-				console.error('Error forking repository: %j', forkError);
-			}
-			console.log('Successfully forked repository: %j', forkResponse);
+});
 
-			// TODO: Clone the new fork on the server and store the path on the User
-		});
+server.get('/github/user', function showUser(req, res) {
+	var github = new GitHubApi(true);
+
+	// TODO: Handle no accessToken
+	github.authenticateOAuth(req.session.accessToken);
+	
+	github.getUserApi().show('', function handleShowUserResponse(userErr, userResponse) {
+		var user;
+
+		if (userErr) {
+			console.error('Error getting user: %j', userErr);
+			res.redirect('/github/error');
+			return;
+		}
+
+		console.log('Received user info: %j', userResponse);
+
+		var user = new User(req.session.user.code);
+		user.setUserInfo(userResponse);
+		res.render('user.jade', { locals: { user: user } });
+	});
+
+});
+
+server.get('/github/fork', function createFork(req, res) {
+	var github = new GitHubApi(true);
+
+	// TODO: Handle no accessToken
+	github.authenticateOAuth(req.session.accessToken);
+
+	// TODO: Check whether the user has already forked the repository
+	// TODO: Get the repository to fork from config
+	github.getRequest().post('repos/fork/7digital/7digital-python-api-wrapper',
+								{} /* postParameters */, null /* requestOptions */,
+								function handlerForkResponse(forkError, forkResponse) {
+		if (forkError) {
+			console.error('Error forking repository: %j', forkError);
+		}
+		console.log('Successfully forked repository: %j', forkResponse);
+
+		// TODO: Clone the new fork on the server and store the path on the User
 	});
 
 });
